84cfd6e223c2763146c813f247f4aee289b56c72
ike-re <67263156+ike-re@users.noreply.github.com>
2020-08-11 17:29:16 +0900

    ajax記述

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,32 @@
+$(function () {
+  function buildHTML(comment) {
+    var html = `<div class="commentBlock" data-comment-id="${comment}">
+                  <div class="one-block">
+                    ${comment.nickname}
+                  </div>
+                    <div class="item-comment">
+                     ${comment.comment}
+                    </div>    
+                </div >`
+    return html;
+  }
+  $('#new_comment').on('submit', function (e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function (data) {
+        var html = buildHTML(data);
+        $('.commentBlock').append(html);
+        $('#new_comment')[0].reset();
+        $('input').prop('disabled', false);
+      })
+  });
+});
